c407bcc1e22136926ce30b8614f46af585824f60
EricCaballeroM <88851026+EricCaballeroM@users.noreply.github.com>
2021-08-25 19:52:58 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,5 +1,35 @@
 def main():
     # Escribe el código adecuado para completar el programa
+    #Ordena 3 números
+
+a=int(input("give me the first number: "))
+b=int(input("give me the second number: "))
+c=int(input("give me the third number: "))
+
+if a<b and b<c:
+    print(c)
+    print(b)
+    print(a)
+elif b<a and a<c:
+    print(c)
+    print(a)
+    print(b)
+elif c<b and b<a:
+    print(a)
+    print(b)
+    print(c)
+elif b<c and c<a:
+    print(a)
+    print(c)
+    print(b)
+elif a<c and c<b:
+    print(b)
+    print(c)
+    print(a)
+elif c<a and a<b:
+    print(b)
+    print(a)
+    print(c)
     pass
 
 
